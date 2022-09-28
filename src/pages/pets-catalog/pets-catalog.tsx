9eb7def6381713679e850fed7c01ae973dce19e0
Denys Politov <d.politov@do-it.co>
2022-09-28 17:08:53 +0300

    Added select component

--- src/pages/pets-catalog/pets-catalog.tsx
+++ src/pages/pets-catalog/pets-catalog.tsx
@@ -1,30 +1,39 @@
-import { Pagination } from "components/pagination"
-import { Tabs } from "components/tabs"
-import { useState } from "react"
+import { Pagination } from 'components/pagination'
+import { Select } from 'components/select'
+import { Tabs } from 'components/tabs'
+import { useState } from 'react'
 
 export const PetsCatalog = () => {
-    const petKinds = [
-        { label: 'Cats', value: 'cats' },
-        { label: 'Dogs', value: 'dogs' },
-        { label: 'Hamsters', value: 'hamsters' },
-    ]
+  const petKinds = [
+    { label: 'Cats', value: 'cats' },
+    { label: 'Dogs', value: 'dogs' },
+    { label: 'Hamsters', value: 'hamsters' },
+  ]
 
-    const sortTypes = [
-        { label: 'Sort by name', value: 'name' },
-        { label: 'Sort by price', value: 'price' },
-        { label: 'Sort by age', value: 'age' },
-    ]
+  const sortTypes = [
+    { label: 'Sort by name', value: 'name' },
+    { label: 'Sort by price', value: 'price' },
+    { label: 'Sort by age', value: 'age' },
+  ]
 
-    const [ petKind, setPetKind ] = useState(petKinds[0].value);
-    const [ sortType, setSortType ] = useState(sortTypes[0].value);
-    const [ page, setPage ] = useState(6);
-    const pagesCount = 10;
+  const selects = ['First', 'Second', 'Third']
 
-    return (
-        <>
-            <Tabs currentTab={petKind} tabs={petKinds} onChange={setPetKind}/>
-            <Tabs currentTab={sortType} tabs={sortTypes} onChange={setSortType}/>
-            <Pagination currentPage={page} pagesCount={pagesCount} onChange={setPage}/>
-        </>
-    )
+  const [petKind, setPetKind] = useState(petKinds[0].value)
+  const [sortType, setSortType] = useState(sortTypes[0].value)
+  const [page, setPage] = useState(6)
+  const [select, setSelect] = useState('')
+  const pagesCount = 10
+
+  return (
+    <>
+      <Tabs currentTab={petKind} tabs={petKinds} onChange={setPetKind} />
+      <Tabs currentTab={sortType} tabs={sortTypes} onChange={setSortType} />
+      <Pagination
+        currentPage={page}
+        pagesCount={pagesCount}
+        onChange={setPage}
+      />
+      <Select title='Try me' currentValue={select} values={selects} onChange={setSelect}/>
+    </>
+  )
 }
