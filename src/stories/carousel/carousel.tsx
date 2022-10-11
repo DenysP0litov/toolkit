a37205116ffe5f1129d5cdf17a7d3b5be8549a65
Denys Politov <d.politov@do-it.co>
2022-10-12 02:37:41 +0300

    Changed changeSlide func

--- src/stories/carousel/carousel.tsx
+++ src/stories/carousel/carousel.tsx
@@ -2,62 +2,45 @@ import { Children, FC, ReactNode, useState } from 'react'
 import './carousel.scss'
 
 type Props = {
-    children: ReactNode
+  children: ReactNode
 }
 
-export const Carousel: FC<Props> = ({children}) => {
-    const [slide, setSlide] = useState(1)
-    const slides = Children.toArray(children)
-    const slidesAmount = slides.length
+export const Carousel: FC<Props> = ({ children }) => {
+  const [slide, setSlide] = useState(1)
+  const slides = Children.toArray(children)
+  const slidesAmount = slides.length
 
-    const changeSlide = (direction: string) => {
-        switch (direction) {
-            case('prev'):
-                if (slide !== 1) {
-                    setSlide(slide - 1)
-                }
-            break
-            case ('next'):
-                if (slide !== slidesAmount) {
-                    setSlide(slide + 1)
-                }
-            break
-            default:
-        }
+  const changeSlide = (direction: 'prev' | 'next') => {
+    if (direction === 'prev' && slide !== 1) {
+      setSlide(slide - 1)
+    } else if (direction === 'next' && slide !== slidesAmount) {
+      setSlide(slide + 1)
     }
+  }
 
-    return (
-        <div className="carousel">
-            <button 
-                className="carousel__button"
-                onClick={() => changeSlide('prev')}
-            >
-                {'<'}
-            </button>
-            <div  className="carousel__slide-container">
-                <div 
-                    className="carousel__slides"
-                    style={{
-                        width: `${100 * slidesAmount}%`,
-                        transform: `translateX(${-(100 / slidesAmount) * (slide - 1)}%)`
-                    }}
-                >
-                    {slides.map((slide, index) => 
-                        <div 
-                            className="carousel__slide"
-                            key={index}
-                        >
-                            {slide}
-                        </div>
-                    )}
-                </div>
+  return (
+    <div className="carousel">
+      <button className="carousel__button" onClick={() => changeSlide('prev')}>
+        {'<'}
+      </button>
+      <div className="carousel__slide-container">
+        <div
+          className="carousel__slides"
+          style={{
+            width: `${100 * slidesAmount}%`,
+            transform: `translateX(${-(100 / slidesAmount) * (slide - 1)}%)`,
+          }}
+        >
+          {slides.map((slide, index) => (
+            <div className="carousel__slide" key={index}>
+              {slide}
             </div>
-            <button 
-                className="carousel__button"
-                onClick={() => changeSlide('next')}
-            >
-                {'>'}
-            </button>
+          ))}
         </div>
-    )
+      </div>
+      <button className="carousel__button" onClick={() => changeSlide('next')}>
+        {'>'}
+      </button>
+    </div>
+  )
 }
